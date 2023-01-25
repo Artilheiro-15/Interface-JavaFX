c70f6497e58c770cd3ff445ceca8c917d2b853ad
Atila jr <atilla.junior33@gmail.com>
2023-01-25 12:12:30 -0300

    Segundo commit

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -10,6 +12,6 @@ public class ViewController {
 	
 	@FXML
 	public void  onBtTestAction() {
-		System.out.println("Click");
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR);
 	}
 }
